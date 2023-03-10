03351a0e1190423702d42bf0507a4b828fa10190
Vadym Kolesnyk <vadym.kolesnyk@binary-studio.com>
2023-03-10 21:20:50 +0200

    Fix body

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -5,27 +5,24 @@ var app = builder.Build();
 
 app.Use(async (context, next) =>
 {
-    Console.WriteLine(context.Request.GetDetails());
+    Console.WriteLine(await context.Request.GetDetails());
     await next.Invoke();
 });
 app.Run();
 
 public static class HttpRequestExtensions
 {
-    public static string GetDetails(this HttpRequest request)
+    public static async Task<string> GetDetails(this HttpRequest request)
     {
         var baseUrl = $"{request.Scheme}://{request.Host}{request.Path}{request.QueryString.Value}";
         var sbHeaders = new StringBuilder();
         foreach (var header in request.Headers)
             sbHeaders.Append($"{header.Key}: {header.Value}\n");
 
-        var body = "no-body";
-        if (request.Body.CanSeek)
-        {
-            request.Body.Seek(0, SeekOrigin.Begin);
-            using var sr = new StreamReader(request.Body);
-            body = sr.ReadToEnd();
-        }
+        using var sr = new StreamReader(request.Body);
+        var body =  await sr.ReadToEndAsync();
+        
+        body = body is null or "" ? "no-body" : body;
 
         return $"{request.Protocol} {request.Method} {baseUrl}\n\n{sbHeaders}\n{body}";
     }
